9456729140d168aaee24e3696f9e3d32cddeb898
shubhamBorkute <borkuteshubh@gmail.com>
2025-02-19 18:59:34 +0530

    task 01

---
+++ BE_Task01/src/main/java/com/workfall/BeTask01Application.java
@@ -0,0 +1,13 @@
+package com.workfall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeTask01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeTask01Application.class, args);
+	}
+
+}
---
+++ BE_Task01/src/test/java/com/workfall/BeTask01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workfall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeTask01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
